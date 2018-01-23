d46d02fd5418d370be615a15e86444a2d2b6ec94
GauthamPughaz <gauthampughazhendhi@gmail.com>
2018-01-23 23:39:08 +0530

    Added feed support.

--- earthosys_site/home/views.py
+++ earthosys_site/home/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.views import View
+from .models import FeedPrediction
+from predictor.models import PredictorRecord
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class HomeView(View):
     def get(self, request):
         pass
 
     def post(self, request):
-        pass
+        feeds, records, counter = {}, {}, 0
+        predicted_feeds = FeedPrediction.objects.all().order_by('-id').values()[:5]
+        for feed in predicted_feeds:
+            feeds[counter] = feed
+            counter += 1
+        counter = 0
+        predictor_records = PredictorRecord.objects.all().order_by('-id').values()[:5]
+        for record in predictor_records:
+            records[counter] = record
+            counter += 1
+        return JsonResponse({'feeds': feeds, 'records': records})
